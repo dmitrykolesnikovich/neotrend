16bdebfe2d4e6e031ad5af655e710a69e47a4733
Dmitry Kolesnikovich <dmitrykolesnikovich@users.noreply.github.com>
2025-04-07 08:21:39 +0300

    fix sheets

--- common/src/commonMain/kotlin/main.kt
+++ common/src/commonMain/kotlin/main.kt
@@ -4,10 +4,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.*
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,7 +29,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
-import site.neotrend.SheetStep.*
+import site.neotrend.SheetStep.INITIAL
+import site.neotrend.SheetStep.MESSAGE
+import site.neotrend.SheetStep.PAYMENT
+import site.neotrend.SheetStep.SUCCESS
 import site.neotrend.platform.VideoPlayer
 
 @Composable
@@ -93,7 +110,7 @@ private fun player(navigation: Navigation) {
         fun updateState(newState: SheetStep) = coroutineScope.launch {
             sheetState.hide()
             sheetStep = newState
-            if (sheetStep != INACTIVE) {
+            if (sheetStep != INITIAL) {
                 sheetState.show()
             }
         }
@@ -102,11 +119,11 @@ private fun player(navigation: Navigation) {
             sheetState = sheetState,
             sheetShape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp),
             sheetContent = {
-                IconButton(onClick = { updateState(INACTIVE) }) {
+                IconButton(onClick = { updateState(INITIAL) }) {
                     Icon(Icons.Default.Close, contentDescription = null)
                 }
                 when (sheetStep) {
-                    INACTIVE -> {
+                    INITIAL -> {
                         // hidden
                     }
                     PAYMENT -> {
@@ -119,21 +136,21 @@ private fun player(navigation: Navigation) {
                     }
                     SUCCESS -> {
                         Text("SUCCESS")
-                        Button(onClick = { updateState(INACTIVE) }) { Text("START") }
+                        Button(onClick = { updateState(INITIAL) }) { Text("INITIAL") }
                     }
                 }
             }
         ) {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 VideoPlayer(modifier = Modifier.fillMaxSize(), appState.author.fileName)
-                Button(onClick = { coroutineScope.launch { sheetStep = PAYMENT; sheetState.show() } }) { Text("Заказать обзор у блогера") }
+                Button(onClick = { coroutineScope.launch { updateState(PAYMENT) } }) { Text("Заказать обзор у блогера") }
             }
         }
     }
 }
 
 private enum class SheetStep {
-    INACTIVE,
+    INITIAL,
     PAYMENT,
     MESSAGE,
     SUCCESS,
